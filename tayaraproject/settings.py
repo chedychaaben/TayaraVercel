f312bac26c89f7b18202537bfa5aed31827a962a
Chedy <chedychaaben@gmail.com>
2022-10-08 12:38:39 +0100

    Update CORS Allowed

--- tayaraproject/settings.py
+++ tayaraproject/settings.py
@@ -217,4 +217,9 @@ CLOUDINARY_STORAGE = {
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage' #this adds as storage in every image or file model line //Three options (Image,Raw(pdf,txt),Video)
 
 # CORS For React
-CORS_ALLOWED_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
