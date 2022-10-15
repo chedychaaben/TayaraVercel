4ce4e1b1b6a1b26ea3ae7c230d4fac1ff940946b
Chedy Chaaben <40511388+chedychaaben@users.noreply.github.com>
2022-10-15 13:46:34 +0100

    update vercel.json

--- tayaraproject/settings.py
+++ tayaraproject/settings.py
@@ -190,7 +190,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'react-build/static'),
+    os.path.join(BASE_DIR,'react-build', 'static'),
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR ,'staticfiles_build_vercel')
