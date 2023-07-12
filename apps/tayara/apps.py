b3cdc1fba0d818728fddc7ef4a79b281be63eab1
Chedy Chaaben <40511388+chedychaaben@users.noreply.github.com>
2023-07-12 14:28:48 +0100

    removing the apscheduler

--- apps/tayara/apps.py
+++ apps/tayara/apps.py
@@ -5,6 +5,7 @@ from jobs import updater
 class TayaraConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.tayara'
-
+    '''
     def ready(self):
     	updater.start()
+    '''
